95e3b3515d4bfd3c9d8f86f4a205b32999b58dc6
SmatMan <gamingajun10@gmail.com>
2021-06-01 13:30:32 -0400

    update timeline to 80 posts instead of 10

--- tweetia/src/App.js
+++ tweetia/src/App.js
@@ -154,7 +154,7 @@ function App() {
 
   async function fetchData() {
     var parsedData = [];
-    await fetch("http://127.0.0.1:5000/timeline/10")
+    await fetch("http://127.0.0.1:5000/timeline/80")
       .then((response) => response.json())
       .then((response) => {
         for (var i in response) {
